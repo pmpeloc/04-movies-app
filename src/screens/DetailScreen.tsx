ceaed60f3f1a6f7c84d46531e2c8204d883eea12
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-11-29 16:12:18 -0300

    feat: Go back button added

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -8,18 +8,20 @@ import {
   ScrollView,
   Text,
   ActivityIndicator,
+  TouchableOpacity,
 } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 
 import { RootStackParams } from '../navigation/Navigation';
 import { useMovieDetails } from '../hooks/useMovieDetails';
 import { MovieDetails } from '../components/MovieDetails';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> {}
 
 const screenHeight = Dimensions.get('screen').height;
 
-export const DetailScreen = ({ route }: Props) => {
+export const DetailScreen = ({ route, navigation }: Props) => {
   const movie = route.params;
   const uri = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
 
@@ -41,6 +43,12 @@ export const DetailScreen = ({ route }: Props) => {
       ) : (
         <MovieDetails movieDetail={movieDetail!} cast={cast} />
       )}
+      {/* Close Button */}
+      <TouchableOpacity
+        style={styles.backButton}
+        onPress={() => navigation.pop()}>
+        <Icon color="white" name="arrow-back-outline" size={50} />
+      </TouchableOpacity>
     </ScrollView>
   );
 };
@@ -80,5 +88,12 @@ const styles = StyleSheet.create({
   fontTitle: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  backButton: {
+    position: 'absolute',
+    zIndex: 999,
+    elevation: 9,
+    top: 10,
+    left: 5,
   },
 });
